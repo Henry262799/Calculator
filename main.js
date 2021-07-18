5cef77f848b75012d2e29debeeeb2b282e8662f7
henry <henrychen262799@gmail.com>
2021-07-17 20:55:16 -0400

    The calculator is fully functional and the delete and clear button work as they should and some minor styling and class names were changed

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+//DOM Element References
+
 const display_content=document.querySelector('.display_content');
 const calcButtons=document.querySelectorAll('.calculator-button');
+const operatorButtons=document.querySelectorAll('.operator-button');
 const deleteButton=document.querySelector('.delete_button');
+const clearButton=document.querySelector('.clear_button');
+const equalButton=document.querySelector('.operator-button-equals');
 
+
+// Global Variables to store content
 let stored_value="";
+let stored_operation="";
 display_content.textContent="";
+let operatorClicked=false;
 
 
 
@@ -28,6 +37,22 @@ deleteButton.addEventListener('click', event=>{
 
 //Event Listener for the operator buttons excluding equals
 
+operatorButtons.forEach(button=>{
+    button.addEventListener('click', event=>{
+        operator_clicked(button);
+    })
+})
+
+//Event Listener for the clear button
+
+clearButton.addEventListener('click', event=>{
+    clearAll();
+})
+
+//Event Listener for equals Button
+equalButton.addEventListener('click',event=>{
+    equalExecution();
+})
 
 /**
  * This method updates the current display
@@ -42,9 +67,20 @@ function updateDisplay(input){
     if(display_content.textContent.includes('.') && value==='.'){
         display_content.textContent=display_content.textContent;
     }
-    else{
+    else if(stored_value.length===0){
         display_content.textContent=display_content.textContent + value;
     }
+
+    if(operatorClicked){
+        display_content.textContent="";
+        display_content.textContent=display_content.textContent+value;
+    }
+    else if(stored_value.length>0){
+        display_content.textContent=display_content.textContent+value;
+    }
+
+    operatorClicked=false;
+    
 }
 
 /**
@@ -59,18 +95,79 @@ function delete_method_calc(){
         return;
     }
 
+    if(display_content.textContent==stored_value){
+        stored_value="";
+    }
+
     display_content.textContent=display_content.textContent.substring(0,display_content.textContent.length-1);
 
 }
 
-function operateor_clicked(input){
+
+/**
+ * This method executes the function of what happens when operator buttons are clicked.
+ * @param {Element} input This element contains the operation that was clicked.
+ * @returns 
+ */
+function operator_clicked(input){
+
+    operatorClicked=true;
 
     //Check if the displayed textContent has a number
     if(!hasNumber(display_content.textContent)){
+        //console.log('no number')
         return;
     }
 
-    console.log('here');
+    //console.log(store_operation);
+    //console.log(stored_value);
+    //console.log('here');
+
+    if(stored_value===display_content.textContent ){
+        stored_value=display_content.textContent;
+    }
+
+    if(hasNumber(display_content.textContent) && stored_value.length>0 && operatorClicked){
+        display_content.textContent=operate(stored_operation,stored_value,display_content.textContent);
+        
+        stored_operation=input.getAttribute('data-value');
+        stored_value=display_content.textContent;
+        //console.log(stored_value);
+    
+    }
+    else{
+        //console.log(stored_value);
+        stored_value=display_content.textContent;
+        stored_operation=input.getAttribute('data-value');
+
+    }
+
+
+
+}
+
+
+/**
+ * This method provides us the results of the operations.
+ * @returns This method returns only when the there is no text content
+ */
+function equalExecution(){
+
+    if(!hasNumber(display_content.textContent)){
+        //console.log('no number')
+        return;
+    }
+
+    if(hasNumber(display_content.textContent) && stored_value.length>0 ){
+        display_content.textContent=operate(stored_operation,stored_value,display_content.textContent);
+
+    }
+    //display_content.textContent=stored_value;
+    stored_value=display_content.textContent;
+    stored_operation="";
+    operatorClicked=false;
+
+
 }
 
 
@@ -84,7 +181,8 @@ function operateor_clicked(input){
  * @returns {Number} 
  */
 function add(number_1,number_2){
-    return number_1+number_2;
+    let sum=number_1+number_2;
+    return ""+sum;
 }
 
 
@@ -95,7 +193,8 @@ function add(number_1,number_2){
  * @returns {Number}
  */
 function subtract(number_1,number_2){
-    return number_1-number_2;
+    let difference=number_1-number_2;
+    return ""+difference;
 }
 
 
@@ -107,7 +206,8 @@ function subtract(number_1,number_2){
  * @returns {Number}
  */
 function multiply(number_1,number_2){
-    return number_1*number_2;
+    let product=number_1*number_2;
+    return ""+product;
 }
 
 
@@ -118,26 +218,58 @@ function multiply(number_1,number_2){
  * @returns {Number}
  */
 function divide(number_1,number_2){
-    return number_1/number_2;
+
+    if(number_2===0){return "ERROR PLEASE CLEAR"}
+    let quotient=number_1/number_2;
+
+    //console.log(quotient);
+    return ""+quotient;
 }
 
 
+/**
+ * Operate takes in one of four operations and then returns the evaluated results.
+ * @param {String} operation 
+ * @param {String} number_1 
+ * @param {String} number_2 
+ * @returns a number in the form of a string.
+ */
 function operate(operation,number_1,number_2){
 
-    if(operation==="add"){
-        return add(number_1,number_2);
+    console.log(operation);
+
+    if(operation==="+"){
+        return add(parseInt(number_1),parseInt(number_2));
     }
-    else if(operation==="subtract"){
-        return subtract(number_1,number_2);
+    else if(operation==="-"){
+        return subtract(parseInt(number_1),parseInt(number_2));
     }
-    else if(operation==="multiply"){
-        return multiply(number_1,number_2);
+    else if(operation==="x"){
+        return multiply(parseInt(number_1),parseInt(number_2));
     }
-    else if(operation==="divide"){
-        return divide(number_1,number_2);
+    else if(operation==="/"){
+        return divide(parseInt(number_1),parseInt(number_2));
     }
 }
 
+
+/**
+ * Returns a boolean value stating whether the given string has a number or not.
+ * @param {String} myString 
+ * @returns a true or false value
+ */
 function hasNumber(myString) {
     return /\d/.test(myString);
   }
+
+/**
+ * This funtion resets the calculator to its default state.
+ */
+function clearAll(){
+
+    display_content.textContent="";
+    stored_value="";
+    stored_operation="";
+    operatorClicked=false;
+}
+
